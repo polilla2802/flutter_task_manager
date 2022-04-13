3f533f27253f60fcc06335479cdf8af7a8fcd058
Eduardo Hidalgo <polilla2802@gmail.com>
2022-04-12 23:02:02 -0500

    Se crear los providers de tasks
    Se crean los endpoints que utilizan GET, POST, PUT y DELETE

--- lib/app/providers/task.dart
+++ lib/app/providers/task.dart
@@ -1,4 +1,12 @@
+import 'package:flutter_task_manager/app/models/requests/get_task.dart';
 import 'package:flutter_task_manager/services/api/index.dart';
+
+class TaskArgs {
+  TaskRequest? taskRequest;
+  String? token;
+  int? taskId;
+  TaskArgs({this.taskRequest, this.token, this.taskId});
+}
 
 class TaskProvider {
   late API _api;
@@ -7,13 +15,72 @@ class TaskProvider {
     _api = API();
   }
 
-  Future<ApiResult> getAllTasks(String token) async {
-    try {
-      const String url = "/vdev/tasks-challenge/tasks";
+  Future<ApiResult> getAllTasksByToken(TaskArgs args) async {
+    const String url = "/vdev/tasks-challenge/tasks";
 
+    try {
       return await _api.get(ApiRequest(
-          ApiChannel.nextline, "getAllTasks", url, AuthType.bearer,
-          queryParameters: {"token": token}));
+          ApiChannel.nextline, "getAllTasksByToken", url, AuthType.bearer,
+          queryParameters: {"token": args.token}));
+    } catch (e) {
+      return ApiResult(false);
+    }
+  }
+
+  Future<ApiResult> postTaskByToken(TaskArgs args) async {
+    var putBody = args.taskRequest!.toJson();
+    const String url = "/vdev/tasks-challenge/tasks";
+
+    try {
+      return await _api.post(ApiRequest(
+        ApiChannel.nextline,
+        "postTaskByToken",
+        url,
+        AuthType.bearer,
+        body: putBody,
+      ));
+    } catch (e) {
+      return ApiResult(false);
+    }
+  }
+
+  Future<ApiResult> getTaskById(TaskArgs args) async {
+    const String url = "/vdev/tasks-challenge/tasks/task_id";
+
+    try {
+      return await _api.get(ApiRequest(
+          ApiChannel.nextline, "getTaskById", url, AuthType.bearer,
+          queryParameters: {"task_id": args.taskId.toString()}));
+    } catch (e) {
+      return ApiResult(false);
+    }
+  }
+
+  Future<ApiResult> putTaskById(TaskArgs args) async {
+    var putBody = args.taskRequest!.toJson();
+    const String url = "/vdev/tasks-challenge/tasks/task_id";
+
+    try {
+      return await _api.put(ApiRequest(
+        ApiChannel.nextline,
+        "putTaskById",
+        url,
+        AuthType.bearer,
+        body: putBody,
+      ));
+    } catch (e) {
+      return ApiResult(false);
+    }
+  }
+
+  Future<ApiResult> deleteTaskById(TaskArgs args) async {
+    var putBody = {"token": args.token};
+    const String url = "/vdev/tasks-challenge/tasks/task_id";
+
+    try {
+      return await _api.delete(ApiRequest(
+          ApiChannel.nextline, "deleteTaskById", url, AuthType.bearer,
+          body: putBody, queryParameters: {"task_id": args.taskId}));
     } catch (e) {
       return ApiResult(false);
     }
