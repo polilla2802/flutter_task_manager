69fedf9982e943b19698b77b652fca7cabbfccc5
jose <polilla2802@gmail.com>
2022-04-13 01:35:46 -0500

    Se agrega la lógica al task screen con su provider

--- lib/app/presentation/screens/tasks_screen.dart
+++ lib/app/presentation/screens/tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task_manager/app/controllers/tasks_controller.dart';
 import 'package:flutter_task_manager/app/models/core/result.dart';
 import 'package:flutter_task_manager/app/models/requests/get_task.dart';
 import 'package:flutter_task_manager/app/models/responses/create_task.dart';
@@ -7,27 +8,27 @@ import 'package:flutter_task_manager/app/models/responses/put_task.dart';
 import 'package:flutter_task_manager/app/models/tasks/Task.dart';
 import 'package:flutter_task_manager/app/providers/task.dart';
 import 'package:flutter_task_manager/app/repos/task.dart';
+import 'package:provider/provider.dart';
 
 class TasksScreen extends StatefulWidget {
   static const String tasks_screen_key = "/tasks_screen";
+  final String? userName;
 
-  TasksScreen({Key? key}) : super(key: key);
+  TasksScreen({Key? key, this.userName}) : super(key: key);
 
   @override
-  State<TasksScreen> createState() => _TasksScreenState(tasks_screen_key);
+  State<TasksScreen> createState() =>
+      _TasksScreenState(tasks_screen_key, userName);
 }
 
 class _TasksScreenState extends State<TasksScreen> {
   String? _key;
+  late TaskRepo tasksRepo;
+  late TasksController _tasksController;
 
-  TaskRepo? tasksRepo;
-  int? _id;
-  String _title = "";
-  bool? _isCompleted;
-  String? _dueDate;
-
-  _TasksScreenState(String tasks_screen_key) {
+  _TasksScreenState(String tasks_screen_key, String? userName) {
     _key = tasks_screen_key;
+    _tasksController = TasksController(userToken: userName!);
     tasksRepo = TaskRepo();
   }
 
@@ -39,64 +40,111 @@ class _TasksScreenState extends State<TasksScreen> {
         .addPostFrameCallback((_) async => await _afterBuild());
   }
 
-  Future<void> _afterBuild() async {}
-
-  Future<void> _getTasksByToken({String? token}) async {
-    Result<List<Task>> tasks =
-        await tasksRepo!.getAllTasksByToken(TaskArgs(token: token));
-    setState(() {
-      _title = tasks.data![0].title!;
-    });
+  Future<void> _afterBuild() async {
+    await _tasksController.getTaskList();
   }
 
-  Future<void> _postTask(TaskRequest task) async {
-    Result<CreateTaskResponse> tasks =
-        await tasksRepo!.postTaskByToken(TaskArgs(taskRequest: task));
-    print("task created ${tasks.data!.detail}");
+  Future<void> _postTask({TaskRequest? task}) async {
+    await _tasksController.postTask(TaskArgs(taskRequest: task));
   }
 
   Future<void> _getTaskById({int? taskId}) async {
-    Result<Task> task = await tasksRepo!.getTaskById(TaskArgs(taskId: taskId));
+    Result<Task> task = await tasksRepo.getTaskById(TaskArgs(taskId: taskId));
   }
 
   Future<void> _putTaskById(TaskRequest task) async {
     Result<PutTaskResponse> tasks =
-        await tasksRepo!.putTaskById(TaskArgs(taskRequest: task));
+        await tasksRepo.putTaskById(TaskArgs(taskRequest: task));
   }
 
   Future<void> _deleteTaskById(TaskRequest task) async {
     Result<DeleteTaskResponse> tasks =
-        await tasksRepo!.deleteTaskById(TaskArgs(taskRequest: task));
+        await tasksRepo.deleteTaskById(TaskArgs(taskRequest: task));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_key!),
-      ),
-      body: Center(
-        child: Column(
+    return ChangeNotifierProvider(
+        create: (context) => _tasksController,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(_tasksController.userToken),
+          ),
+          body: _buildBody(),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => _postTask(
+                task: TaskRequest(
+                    title: "test",
+                    isCompleted: 0,
+                    dueDate: "2022-04-12",
+                    comments: "test",
+                    description: "test",
+                    tags: "test",
+                    token: _tasksController.userToken)),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ));
+  }
+
+  Widget _buildBody() {
+    return Consumer<TasksController>(
+      builder: (context, _tasksController, child) {
+        switch (_tasksController.state) {
+          case TasksState.initial:
+            return Container(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text("holi")],
+                ));
+          case TasksState.loading:
+            return Container(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [CircularProgressIndicator()],
+                ));
+          case TasksState.success:
+            return Container(alignment: Alignment.center, child: _buildTasks());
+          case TasksState.noTasks:
+            return Container(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text("No tasks sorry")],
+                ));
+          default:
+            return Container(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text("crayoli")],
+                ));
+        }
+      },
+    );
+  }
+
+  Widget _buildTasks() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: _tasksController.tasks!
+          .asMap()
+          .map((i, item) => MapEntry(
+                i,
+                _getTask(i, item),
+              ))
+          .values
+          .toList(),
+    );
+  }
+
+  Widget _getTask(int id, Task task) {
+    return Container(
+      child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              _title,
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _postTask(TaskRequest(
-            title: "test",
-            isCompleted: 0,
-            dueDate: "2022-04-12",
-            comments: "test",
-            description: "test",
-            tags: "test",
-            token: "Joe123")),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+          children: [Text(task.id.toString())]),
     );
   }
 }
