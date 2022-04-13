4c72ce7829edcbf1b1022ab46ae5aa8a03f4047a
Eduardo Hidalgo <polilla2802@gmail.com>
2022-04-12 23:04:20 -0500

    Se crea el objeto Task

---
+++ lib/app/models/tasks/Task.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_task_manager/app/models/tasks/TaskEntity.dart';
+
+class Task {
+  final int? id;
+  final String? title;
+  final int? isCompleted;
+  final String? dueDate;
+
+  Task({this.id, this.title, this.isCompleted, this.dueDate});
+
+  Task.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        isCompleted = json['is_completed'],
+        dueDate = json['due_date'];
+
+  TaskEntity toTaskEntity() {
+    return TaskEntity(
+        id: id, title: title, isCompleted: isCompleted, dueDate: dueDate);
+  }
+}
